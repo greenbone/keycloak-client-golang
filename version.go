ab25b9d770b5b74ff542f342e15f54d6383d5fb1
Salim Nassim <salim.nassim@gmail.com>
2024-07-25 15:36:31 +0200

    fix: autogenerated yet wrong, use AGPL

--- version.go
+++ version.go
@@ -1,9 +1,5 @@
 // SPDX-FileCopyrightText: 2023 Greenbone AG
 //
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-// Copyright (C) Greenbone Networks GmbH
-//
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package keycloak_client_golang
